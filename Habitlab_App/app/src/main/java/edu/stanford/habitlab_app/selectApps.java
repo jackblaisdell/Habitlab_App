9e91d861ca4f456058e050e62bd2f953e33732b7
radhajain <jainr@stanford.edu>
2017-06-26 14:54:07 -0700

    First page

---
+++ Habitlab_App/app/src/main/java/edu/stanford/habitlab_app/selectApps.java
@@ -0,0 +1,13 @@
+package edu.stanford.habitlab_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class selectApps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_apps);
+    }
+}
